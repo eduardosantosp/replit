7a08fa734e78192647e52eb709eef2e7d9734914
eduardosantosp <130381385+eduardosantosp@users.noreply.github.com>
2024-10-18 22:26:26 -0300

    Add files via upload

---
+++ Nodejs-ledson/index.js
@@ -0,0 +1,17 @@
+const http = require('http')
+
+console.dir(http)
+
+/*
+const arr = [1,5,6,8,12,32,3]
+console.trable(arr)
+console.dir(arr)
+console.error("Error!")
+console.warning("Attemptino")
+console.infor(arr)
+console.time
+
+console.log(arr)
+
+console.time()
+*/
